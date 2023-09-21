cfa77d89bf39e4f03a64f53955c64b39d2e4f7a8
JimiPerez <Jimiperez412@gmail.com>
2023-09-21 15:36:20 -0500

    Removed excess code from public/src/home.js

--- public/src/home.js
+++ public/src/home.js
@@ -1,23 +1,4 @@
-function getTotalBooksCount(books) {}
 
-function getTotalAccountsCount(accounts) {}
-
-function getBooksBorrowedCount(books) {}
-
-function getMostCommonGenres(books) {}
-
-function getMostPopularBooks(books) {}
-
-function getMostPopularAuthors(books, authors) {}
-
-module.exports = {
-  getTotalBooksCount,
-  getTotalAccountsCount,
-  getBooksBorrowedCount,
-  getMostCommonGenres,
-  getMostPopularBooks,
-  getMostPopularAuthors,
-};
 function getTotalBooksCount(books) {
   return books.length;
 }
